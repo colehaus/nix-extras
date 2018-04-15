4c7c45e90e58470cb886d341b7233fa06dd5f42e
Cole Haus <colehaus@cryptolab.net>
2018-04-15 13:04:24 -0700

    Add `callNode2nix`

---
+++ callNode2nix.nix
@@ -0,0 +1,23 @@
+# `postBuild` is a fairly hacky way of accommodating one-off fixes
+{ pkgs ? import <nixpkgs> {}, name, src, postBuild ? "" } :
+  let
+    packageJson = pkgs.writeTextDir "package.json" (builtins.readFile src);
+    node2nix = pkgs.stdenv.mkDerivation {
+      name = "node2nix-${name}";
+      nativeBuildInputs = [ pkgs.nodePackages.node2nix pkgs.nix ];
+      phases = [ "buildPhase" "installPhase" ];
+      src = packageJson;
+      # We need the temporary directory so that `node2nix`'s relative paths are correct
+      buildPhase = ''
+        TMP=$(mktemp -d)
+        cd $TMP
+        node2nix --development -6 --input "$src"/package.json
+        ${postBuild}
+      '';
+      installPhase = ''
+        mkdir -p "$out"
+        cp *.nix "$out"
+      '';
+    };
+  in
+    pkgs.callPackage node2nix {}
--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
 {
   pinnedPkgs = import ./pinned-pkgs.nix;
   callBower2nix = import ./callBower2nix.nix;
+  callNode2nix = import ./callNode2nix.nix;
 }
