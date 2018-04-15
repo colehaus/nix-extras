f00b25c7d0d5000041c6c473785af154e9f74aa0
Cole Haus <colehaus@cryptolab.net>
2018-04-15 13:54:26 -0700

    Add `callNpm`

---
+++ callNpm.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {}, name, versionSpec } :
+  let
+    npm2nix = pkgs.stdenv.mkDerivation {
+      inherit name;
+      nativeBuildInputs = [ pkgs.nodePackages.node2nix pkgs.nix ];
+      phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+      src = pkgs.writeTextDir "node-packages.json" (''
+        [ { "${name}": "${versionSpec}" } ]
+      '');
+      buildPhase = ''
+        node2nix -6 --input node-packages.json
+      '';
+      installPhase = ''
+        mkdir -p "$out"
+        cp *.nix "$out"
+      '';
+    };
+  in
+    (pkgs.callPackage npm2nix {})."${name}-${versionSpec}"
--- default.nix
+++ default.nix
@@ -2,4 +2,5 @@
   pinnedPkgs = import ./pinned-pkgs.nix;
   callBower2nix = import ./callBower2nix.nix;
   callNode2nix = import ./callNode2nix.nix;
+  callNpm = import ./callNpm.nix;
 }
