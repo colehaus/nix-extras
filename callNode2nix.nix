f33ab8f70982073b08c67e0d4f6144d029f0441d
Cole Haus <colehaus@cryptolab.net>
2019-11-19 17:54:31 -0800

    Fix callNode2nix by putting package and lock in build directory
    node2nix expects both package.json and package-lock.json to be in the same directory

--- callNode2nix.nix
+++ callNode2nix.nix
@@ -1,23 +1,18 @@
 # `postBuild` is a fairly hacky way of accommodating one-off fixes
 { pkgs ? import <nixpkgs> {}, name, package, packageLock, postBuild ? "" } :
-  let
-    packageJson = pkgs.writeTextDir "package.json" (builtins.readFile package);
-    packageLockJson = pkgs.writeTextDir "package-lock.json" (builtins.readFile packageLock);
-  in
     pkgs.stdenv.mkDerivation {
       name = "node2nix-${name}";
       nativeBuildInputs = [ pkgs.nodePackages.node2nix pkgs.nix ];
       phases = [ "buildPhase" "installPhase" ];
-      srcs = [ packageJson packageLockJson ];
-      # We need the temporary directory so that `node2nix`'s relative paths are correct
+      srcs = [ package packageLock ];
       buildPhase = ''
-        TMP=$(mktemp -d)
-        cd $TMP
-        node2nix --development --nodejs-10 --input ${packageJson}/package.json --lock ${packageLockJson}/package-lock.json
+        cp ${package} package.json
+        cp ${packageLock} package-lock.json
+        node2nix --development --nodejs-10 --input package.json --lock package-lock.json
         ${postBuild}
       '';
       installPhase = ''
         mkdir -p "$out"
-        cp *.nix "$out"
+        cp package.json package-lock.json *.nix "$out"
       '';
     }
